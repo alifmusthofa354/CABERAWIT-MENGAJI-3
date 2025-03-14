a07ad810701e3feeeaba9baa034653b01cb62044
alifmusthofa354 <muhammad.alif.musthofa354@gmail.com>
2025-03-14 15:11:51 +0700

    testing deploy

--- components/custom/HeaderPage.tsx
+++ components/custom/HeaderPage.tsx
@@ -4,7 +4,7 @@ export default function HeaderPage() {
   return (
     <div className="text-center">
       <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
-        Generus Mengaji
+        Generus Mengaji V3
       </h1>
       <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
         Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
