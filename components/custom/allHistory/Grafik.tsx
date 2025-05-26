a6b85b2a87b704c49924207a0089b13cb3cd4d9c
alifmusthofa354 <muhammad.alif.musthofa354@gmail.com>
2025-05-26 18:31:31 +0700

    change size outer radius

--- components/custom/allHistory/Grafik.tsx
+++ components/custom/allHistory/Grafik.tsx
@@ -11,8 +11,8 @@ import {
 
 const data = [
   { name: "Hadir", value: 5 },
-  { name: "Ijin", value: 1 },
-  { name: "Alfa", value: 1 },
+  { name: "Ijin", value: 5 },
+  { name: "Alfa", value: 5 },
 ];
 
 const COLORS = ["#259c36", "#758577", "#bd2121"];
@@ -21,8 +21,8 @@ export default function Grafik() {
   return (
     <div className="w-full h-[400px] p-8">
       <h1 className="text-2xl font-bold mb-4">Grafik Absensi</h1>
-      <ResponsiveContainer width="100%" height="100%">
-        <PieChart>
+      <ResponsiveContainer className={"w-full h-full"}>
+        <PieChart className={"w-full h-full"}>
           <Pie
             data={data}
             cx="50%"
@@ -31,8 +31,8 @@ export default function Grafik() {
             label={({ name, percent }) =>
               `${name} (${(percent * 100).toFixed(0)}%)`
             }
-            outerRadius={120}
             fill="#8884d8"
+            outerRadius={90}
             dataKey="value"
           >
             {data.map((entry, index) => (
