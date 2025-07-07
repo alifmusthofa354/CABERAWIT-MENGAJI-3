36f4ed82c912d69cd47fbe7f2f879e2833cdf830
alifmusthofa354 <muhammad.alif.musthofa354@gmail.com>
2025-07-07 10:31:31 +0700

    ui absensi kosong v2

--- components/custom/absense/EmptyAttedance.tsx
+++ components/custom/absense/EmptyAttedance.tsx
@@ -21,11 +21,8 @@ export default function EmptyAttedance() {
 
         {/* Konten Utama - Pesan Kosong yang Lebih Menarik */}
         <div className="flex-1 flex flex-col items-center justify-center p-4">
-          <div className="text-center space-y-6">
-            {" "}
-            {/* Menambah space-y untuk jarak antar elemen */}
-            {/* Ilustrasi atau Ikon */}
-            <div className="w-48 h-48 mx-auto">
+          <div className="text-center space-y-3">
+            <div className="w-48 h-48  mx-auto">
               {" "}
               {/* Ukuran ilustrasi */}
               {/* Ganti dengan ilustrasi atau ikon yang relevan */}
@@ -34,19 +31,15 @@ export default function EmptyAttedance() {
                 alt="No attendance data"
                 width={192} // Lebar sesuai w-48 (192px)
                 height={192} // Tinggi sesuai h-48 (192px)
-                className="object-contain"
+                className="object-contain w-full h-full"
               />
             </div>
             {/* Pesan yang Lebih Ramah */}
-            <p className="text-gray-700 text-3xl font-semibold leading-tight">
-              {" "}
-              {/* Font lebih besar dan tebal */}
-              Belum ada absensi tercatat hari ini.
+            <p className="text-gray-700 text-lg sm:text-2xl md:text-3xl font-semibold leading-tight px-4">
+              Belum ada absensi hari ini.
             </p>
-            <p className="text-gray-500 text-lg max-w-md mx-auto">
-              {" "}
-              {/* Pesan pendukung */}
-              Tekan tombol + untuk mulai merekam kehadiran siswa.
+            <p className="text-gray-500 text-sm sm:text-base md:text-lg max-w-sm md:max-w-full mx-auto px-4">
+              Tekan tombol + untuk merekam kehadiran siswa.
             </p>
           </div>
         </div>
